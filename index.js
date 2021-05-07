e555ae7c715573d3728ac4a410e850ace82dc171
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-05-06 23:06:46 -0400

    Pass tests for polygon

--- index.js
+++ index.js
@@ -4,4 +4,19 @@ class Polygon{
     constructor(sides) {
         this.sides = sides;
     }
+
+    get countSides() {
+        return this.sides.length;
+    }
+
+    get perimeter() {
+        return this.sides.reduce( (a,b)=>a+b );
+    }
+}
+
+class Triangle extends Polygon {
+    isValid() {
+        // console.log(this.countSides());
+        return (this.countSides() === 3 ? !!1 : !!0);
+    }
 }
